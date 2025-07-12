b4eeb8ee7c6c947028c1c37df1bb865783d62565
mostafa kandel <101183814+MostafaKandel@users.noreply.github.com>
2025-07-12 23:10:55 +0200

    Q1

--- Session-4-C/Program.cs
+++ Session-4-C/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region question1
+            // Write a program that allows the user to insert an integer then print all numbers between 1 to that number
+           Console.WriteLine("Enter an integer:");
+           bool isValidInput = int.TryParse(Console.ReadLine(), out int number);
+         if (isValidInput && number > 0) { 
+               for (int i = 1; i <= number; i++)
+               {
+                   Console.Write($"{i},");
+               }
+            }
+            #endregion
+
+           
         }
     }
 }
