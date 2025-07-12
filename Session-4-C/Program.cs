1b8a2a3e846029d7fdbf1048df0ef97c4c74e32a
mostafa kandel <101183814+MostafaKandel@users.noreply.github.com>
2025-07-12 23:02:52 +0200

    Add project files.

---
+++ Session-4-C/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Session_4_C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
